5a668dc505cbcedfa1f1e5d0cae3920eab609360
EdbertChan <edbert.chan@yahoo.com>
2014-08-09 12:36:53 -0700

    Initial commit: Facebook login

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def home
+   # @course = current_user.courses.build if signed_in?
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PingServer::Application.config.secret_key_base = 'd6a54907b7c2ce6f50bd0e41bd012fb1a5160fb20ff7bb68e4fc6ac772f7167ae9f10f3bab7f5abd44f937e902eae48175dec8577f1d767c07287bd697b7248d'
